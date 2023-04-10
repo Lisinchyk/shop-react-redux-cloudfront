1f38d9a2237b81452406f40d2c826425b4ce8e7a
Anatolii Lisinchuk <anatolii.lisinchuk@miqdigital.com>
2023-04-10 03:08:09 +0200

    fix

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,8 +3,8 @@ const API_PATHS = {
   order: "https://.execute-api.us-east-1.amazonaws.com/dev",
   import: "https://bg1zgz9ab8.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://z93dh321nh.execute-api.us-east-1.amazonaws.com/dev",
-  // cart: "https://l1rdd08hei.execute-api.us-east-1.amazonaws.com/dev",
-  cart: "http://localhost:3000/dev",
+  cart: "https://l1rdd08hei.execute-api.us-east-1.amazonaws.com/dev",
+  // cart: "http://localhost:3000/dev",
 };
 
 export default API_PATHS;
